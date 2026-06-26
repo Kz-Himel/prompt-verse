ad951601257d29692b18eabc6b7fb651c6a144eb
Kz Himel <kzhimel129@gmail.com>
2026-06-26 18:06:55 +0600

    Implement: JWT to protect private routes

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,22 +1,33 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGODB_URI);
+await client.connect();
 const db = client.db(process.env.AUTH_DB_NAME);
 
 export const auth = betterAuth({
-  emailAndPassword: { 
-    enabled: true, 
+  database: mongodbAdapter(db, { client }),
+
+  emailAndPassword: {
+    enabled: true,
   },
-  database: mongodbAdapter(db, {
-    client
-  }),
-  user: {
-    additionalFields: {
-      role: {
-        defaultValue: "user",
-      }
-    }
-  }
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    },
+  },
+
+  session: {
+    cookieCache: {
+      enabled: true,
+      strategy: "jwt",
+      maxAge: 7 * 24 * 60 * 60,
+    },
+  },
+
+  plugins: [jwt()],
 });
